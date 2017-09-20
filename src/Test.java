82c075a997e64569e7ad955e940bb60bb0a856ef
zouzheng <57830716@qq.com>
2017-09-20 12:05:40 +0800

    试下其他用户上传

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,12 @@
-import java.util.UUID;
 
 public class Test {
 	
 
 	public static void main(String[] args)  {
 		
-		boolean a = true;
-		for(int i = 0;i<10;i++){
-			a = !a;
-			System.out.println(a);
-		}
+		System.out.println(CodeUtil.SHA1Encoder("abc"));
+		System.out.println(CodeUtil.SHA256Encoder("abcd"));
+		System.out.println(CodeUtil.SHA256Encoder("123"));
 		
 		
 	}
